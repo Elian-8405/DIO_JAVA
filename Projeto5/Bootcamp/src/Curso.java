05b93bd7be89c48e4eb6e4d0c461f1f24db55f34
Elia Silva <elianribeiro022@gmail.com>
2024-10-25 16:01:33 -0300

    Projeto5

---
+++ Projeto5/Bootcamp/src/Curso.java
@@ -0,0 +1,10 @@
+public class Curso {
+    //Atributos
+
+    private String titulo;
+    private String descricao;
+    private int cargaHoraria;
+
+
+
+}
---
+++ Projeto5/Bootcamp/src/Mentoria.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+
+public class Mentoria {
+    private String titulo;
+    private String descricao;
+    private LocalDate data;
+
+
+}
